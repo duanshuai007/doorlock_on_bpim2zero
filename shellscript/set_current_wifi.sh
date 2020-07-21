55250ad3bf2e7781b00b03686590529be7986273
duanshuai007 <duanbixing@163.com>
2020-07-21 17:25:04 +0800

    脚本现在能够设置choice选项内容

--- shellscript/set_current_wifi.sh
+++ shellscript/set_current_wifi.sh
@@ -2,11 +2,30 @@
 #该脚本用于调试使用，用来方便的设置的Wi-Fi，该脚本会更新net.conf中的ssid和psk信息
 #ssid 就是wifi名
 #psk 就是wifi密码
-if [ $# -lt 2 ]
-then
-	echo "./set_current_wifi.sh ssid psk"
-	exit
-fi
+set_netconf_option() {
+	case $1 in
+		"choice")
+		if [ $# -lt 2 ]
+		then
+			echo "run like this:"
+			echo "./set_current_wifi.sh choice wlan0/ppp0/eth0"
+			exit
+		fi
+		sed -i "s/\(choice=\).*/\1$2/" net.conf
+		;;
+		"wlan")
+		if [ $# -lt 3 ]
+		then
+			echo "run like this:"
+			echo "./set_current_wifi.sh wlan ssid psk"
+			exit
+		fi
+		sed -i "s/\(ssid=\).*/\1$2/" net.conf
+		sed -i "s/\(psk=\).*/\1$3/" net.conf
+		;;
+		*)
+		;;
+	esac
+}
 
-sed -i "s/\(ssid=\).*/\1$1/" net.conf
-sed -i "s/\(psk=\).*/\1$2/" net.conf
+set_netconf_option $@
