6f9a9c747f5cc704c132af4c4ee172650bdc49e3
duanshuai007 <duanbixing@163.com>
2020-07-21 17:26:13 +0800

    增加修改network_timestamp时间戳的代码

--- shellscript/update_time.sh
+++ shellscript/update_time.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+NETTIMESTAMP=/tmp/network_timestamp
 service ntp stop
 CUR_TIME=$(date "+%Y-%m-%d %H:%M:%S")
 #echo "${CUR_TIME}:ntpupdate ntp.api.bz" >> /var/log/ntpdate.log
@@ -10,3 +11,4 @@ then
 	/usr/sbin/ntpdate cn.pool.ntp.org
 fi
 service ntp start
+echo 0 > ${NETTIMESTAMP}
