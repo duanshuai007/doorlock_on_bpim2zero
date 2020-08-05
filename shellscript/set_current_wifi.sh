78dd8a7b1185ccfcbb667912b4143780fe655c3c
duanshuai007 <duanbixing@163.com>
2020-08-05 15:18:21 +0800

    修复一个不能够正确修改文件内容的bug

--- shellscript/set_current_wifi.sh
+++ shellscript/set_current_wifi.sh
@@ -2,6 +2,9 @@
 #该脚本用于调试使用，用来方便的设置的Wi-Fi，该脚本会更新net.conf中的ssid和psk信息
 #ssid 就是wifi名
 #psk 就是wifi密码
+
+NETFILE="/root/net.conf"
+
 set_netconf_option() {
 	case $1 in
 		"choice")
@@ -11,7 +14,7 @@ set_netconf_option() {
 			echo "./set_current_wifi.sh choice wlan0/ppp0/eth0"
 			exit
 		fi
-		sed -i "s/\(choice=\).*/\1$2/" net.conf
+		sed -i "s/\(choice=\).*/\1$2/"	${NETFILE}
 		;;
 		"wlan")
 		if [ $# -lt 3 ]
@@ -20,8 +23,8 @@ set_netconf_option() {
 			echo "./set_current_wifi.sh wlan ssid psk"
 			exit
 		fi
-		sed -i "s/\(ssid=\).*/\1$2/" net.conf
-		sed -i "s/\(psk=\).*/\1$3/" net.conf
+		sed -i "s/\(ssid=\).*/\1$2/"	${NETFILE}
+		sed -i "s/\(psk=\).*/\1$3/"		${NETFILE}
 		;;
 		*)
 		;;
